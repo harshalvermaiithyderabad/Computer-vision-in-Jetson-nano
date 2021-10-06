26a246b5ae76c6d0e6b0fae75285713a844125ae
Harshal Verma <88970103+harshalvermaiithyderabad@users.noreply.github.com>
2021-10-06 22:48:40 +0530

    Add files via upload

---
+++ Opencv10-colour_bouncing_on_gray.py
@@ -0,0 +1,42 @@
+import cv2
+print(cv2.__version__)
+dispW =1280
+dispH =960
+flip =2
+#camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+#cam = cv2.VideoCapture(camset) for raspberry pi camera
+cam = cv2.VideoCapture('/dev/video0')
+dispW =int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+dispH =int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+BW = int(0.3*dispW)
+BH = int(0.2*dispH)
+
+posX =10
+posY =270
+dx =3
+dy =2
+
+while True:
+    ret, frame = cam.read()
+    
+    roi =frame[posY:posY+BH,posX:posX+BW].copy()
+    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    frame = cv2.cvtColor(frame,cv2.COLOR_GRAY2BGR)
+    frame[posY:posY+BH,posX:posX+BW] = roi
+    cv2.rectangle(frame,(posX,posY),(posX+BW,posY+BH),(255,0,0),3)
+    posX = posX + dx
+    posY = posY + dy
+    if posX<=0 or posX +BW >= dispW:
+        dx =dx*(-1)
+    if posY <= 0 or posY +BH >= dispH:
+        dy =dy*(-1)
+    cv2.imshow("Gray_roi",frame)
+    cv2.moveWindow("Gray_roi",0,0)
+
+    if cv2.waitKey(1)==ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
+
+# the IDE might not respond always, close the idea and start again
